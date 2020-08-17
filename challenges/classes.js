f024232888eb5e5b51c6be3e33a3f3cfa5ee66bb
Avery Burns <a.aburns95@gmail.com>
2020-08-16 23:00:48 -0500

    classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,47 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+// function CuboidMaker(l, w, h) {
+//     this.length = l;
+//     this.width = w;
+//     this.height = h;
+//   };
+//   CuboidMaker.prototype.volume = function() {
+//     cubeVolume = (this.length * this.width) * this.height;
+//     return cubeVolume;
+//   };
+//   CuboidMaker.prototype.surfaceArea = function() {
+//     surfaceArea = 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+//     return surfaceArea;
+//   };
+//   function Cuboid(l, w , h) {
+//     CuboidMaker.call(l, w, h);
+//     this.length = l;
+//     this.width = w;
+//     this.height = h;
+//   };  
+
+
+class CuboidMaker {
+    constructor(attr) {
+        this.length = attr.l;
+        this.width = attr.w;
+        this.height = attr.h;
+    }
+
+    volume() {
+        cubeVolume = (this.length * this.width) * this.height;
+        return cubeVolume;
+    };
+
+    surfaceArea() {
+        surfaceArea = 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+        return surfaceArea;
+    };
+
+};
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
